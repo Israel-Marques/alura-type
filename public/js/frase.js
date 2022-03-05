159d1e7bbc9e00162db6df02e36afb8477952119
israel marques <devisraelmarques@gmail.com>
2022-03-04 23:42:14 -0300

    feat:  implementando  requisição get e  spiner.

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,9 +1,25 @@
 $("#botao-frase").click(fraseAleatoria);
 
-function fraseAleatoria(){
-    $.get("http://localhost:3000/frases", function(data){
+function fraseAleatoria() {
+    $("#spiner").show
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria) //URL errada para simular um problema
+    .fail( function(){
+        $("#erro").toggle(); //ao falhar mostra a mensagem de erro
+    setTimeout(function(){
+        $("#erro").toggle()
+    }, 3000)
+    })
+    .always(function(){
+        $("#spiner").toggle                    ();
+    })
+}
+
+
+    function trocaFraseAleatoria(data){   
        let frase=$(".frase");
        let numeroAleatorio=Math.floor (Math.random() * data.length);
       frase.text(data[numeroAleatorio].texto)
-});
-}
+      atualizaTamanhoFrase()
+      atualizaTempoInicial(data[numeroAleatorio].tempo)
+    }
+   
